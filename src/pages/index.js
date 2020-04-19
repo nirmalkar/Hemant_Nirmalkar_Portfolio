81d80b67a8c503d1f0375a9a9a0fd0238da6580d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-04-20 00:19:36 +0530

    contact info added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Node from '../images/nodejs.svg'
 import react from '../images/react.svg'
 import Gatsby from '../images/gatsby.svg'
 import Mongo from '../images/mongo.svg'
+import MyWork from '../images/mw.jpg'
 
 const IndexPage = () => (
   <div>
@@ -33,18 +34,49 @@ const IndexPage = () => (
         <img className="hero-ht-img" src={MyImg} alt="" />
       </div>
     </div>
-    <div className="row no-gutters">
+    <div className="row no-gutters m-t-my-skills">
       <div className="col-12 text-center">
         <p className="f-s">My Skills</p>
-        <div>
-          <img className="p-5" src={JS} alt="" />
-          <img className="p-5" src={react} alt="" />
-          <img className="p-5" src={Gatsby} alt="" />
-          <img className="p-5" src={Node} alt="" />
-          <img className="p-5" src={Mongo} alt="" />
+        <div className="mt-5">
+          <img className="p-4" src={JS} alt="" />
+          <img className="p-4" src={react} alt="" />
+          <img className="p-4" src={Gatsby} alt="" />
+          <img className="p-4" src={Node} alt="" />
+          <img className="p-4" src={Mongo} alt="" />
         </div>
       </div>
+      <div className="col-12 text-center m-t-my-work">
+        <p className="f-s">My Work</p>
+        <div className="d-flex justify-content-center">
+          <div>
+            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <p className="my-wrk-txt">asdf</p>
+          </div>
+          <div>
+            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <p className="my-wrk-txt">asdf</p>
+
+          </div>
+          <div>
+            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <p className="my-wrk-txt">asdf</p>
+          </div>
+          <div>
+            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <p className="my-wrk-txt">asdf</p>
+          </div>
+        </div>
+        <p className="my-wrk-lnk">SEE MORE<i class="fas fa-arrow-right  mt-2 ml-2" /></p>
+      </div>
     </div>
+    <div className="con-me-txt">Contact Me</div>
+    <div className="mt-4">
+      <span className="con-txt">Contact No </span><span className="con-no">8839948857</span>
+    </div>
+    <div className="mt-4 m-b">
+      <span className="con-txt">Email Addr. </span><span className="con-no">hemantnirmalkar17@gmail.com</span>
+    </div>
+    <i class="fab fa-github fa-10x" />
   </div>
 )
 
