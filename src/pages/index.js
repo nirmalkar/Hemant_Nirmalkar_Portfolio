fcad8220ca9d047de6de9349d353c8e25ddbcf2d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-04-20 14:52:17 +0530

    link and anchor tag active

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.scss"
+
 import MyImg from '../images/me2.jpg'
-import JS from '../images/node.svg'
+import Sass from '../images/sass.svg'
 import Node from '../images/nodejs.svg'
 import react from '../images/react.svg'
 import Gatsby from '../images/gatsby.svg'
@@ -17,15 +20,23 @@ const IndexPage = () => (
       <div className="col-7 hero-ht">
         <div className="name">Hemant Nirmalkar</div>
         <p className="desc-w">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente mollitia nesciunt distinctio laudantium illo incidunt corrupti reprehenderit! Ratione, in soluta tenetur aut impedit, odit, ut quasi iure libero dicta adipisci!</p>
-        <p className="my-wrk-lnk">MY WORK<i class="fas fa-arrow-right  mt-2 ml-2" /></p>
+        <p className="my-wrk-lnk "><Link className="link-styles" to="/work"> MY WORK<i class="fas fa-arrow-right  mt-2 ml-2" /></Link></p>
         <div className="row">
           <div className="col-4 m-social-lnk-toggle">
           </div>
           <div className="col-8 text-right fa-lg m-social-lnk-toggle">
-            <i class="fab fa-github fa-lg p-3" />
-            <i class="fab fa-codepen fa-lg p-3" />
-            <i class="fab fa-linkedin-in fa-lg p-3" />
-            <i class="fab fa-instagram fa-lg p-4" />
+            <a className="link-styles" href="https://github.com/hemantkumar2" target="_blank">
+              <i class="fab fa-github fa-lg p-3 clk-f-a" />
+            </a>
+            <a className="link-styles" href="" target="_blank">
+              <i class="fab fa-codepen fa-lg p-3 clk-f-a" />
+            </a>
+            <a className="link-styles" href="https://www.linkedin.com/in/hemant-nirmalkar/" target="_blank">
+              <i class="fab fa-linkedin-in fa-lg p-3 clk-f-a" />
+            </a>
+            <a className="link-styles" href="https://www.instagram.com/monty_davinci/" target="_blank">
+              <i class="fab fa-instagram fa-lg p-4 clk-f-a" />
+            </a>
           </div>
         </div>
       </div>
@@ -37,35 +48,41 @@ const IndexPage = () => (
       <div className="col-12 text-center">
         <p className="f-s">My Skills</p>
         <div className="mt-5">
-          <img className="p-4" src={JS} alt="" />
-          <img className="p-4" src={react} alt="" />
-          <img className="p-4" src={Gatsby} alt="" />
-          <img className="p-4" src={Node} alt="" />
-          <img className="p-4" src={Mongo} alt="" />
+          <a href="https://sass-lang.com/" target="_blank">
+            <img className="p-4 img-styles" src={Sass} alt="" />
+          </a>
+          <a href="https://reactjs.org/" target="_blank">
+            <img className="p-4 img-styles" src={react} alt="" />
+          </a>
+          <a href="https://www.gatsbyjs.org/" target="_blank">
+            <img className="p-4 img-styles" src={Gatsby} alt="" />
+          </a>
+          <a href="https://nodejs.org/en/" target="_blank">
+            <img className="p-4 img-styles" src={Node} alt="" />
+          </a>
+          <a href="https://www.mongodb.com/" target="_blank">
+            <img className="p-4 img-styles" src={Mongo} alt="" />
+          </a>
         </div>
       </div>
       <div className="col-12 text-center m-t-my-work">
         <p className="f-s">My Work</p>
         <div className="d-flex justify-content-center">
           <div>
-            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <Link to="/project"><img className="img-w-h mr-4 mt-5 img-styles" src={MyWork} alt="" /></Link>
             <p className="my-wrk-txt">asdf</p>
           </div>
           <div>
-            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <img className="img-w-h mr-4 mt-5 img-styles" src={MyWork} alt="" />
             <p className="my-wrk-txt">asdf</p>
 
           </div>
           <div>
-            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
-            <p className="my-wrk-txt">asdf</p>
-          </div>
-          <div>
-            <img className="img-w-h mr-4 mt-5" src={MyWork} alt="" />
+            <img className="img-w-h mr-4 mt-5 img-styles" src={MyWork} alt="" />
             <p className="my-wrk-txt">asdf</p>
           </div>
         </div>
-        <p className="my-wrk-lnk">SEE MORE<i class="fas fa-arrow-right  mt-2 ml-2" /></p>
+        <p className="my-wrk-lnk"><Link className="link-styles" to="/work">SEE MORE<i class="fas fa-arrow-right  mt-2 ml-2" /></Link></p>
       </div>
     </div>
     <div className="con-me-txt">Contact Me</div>
